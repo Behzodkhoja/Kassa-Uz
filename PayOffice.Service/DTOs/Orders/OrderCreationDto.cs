002df1e1d46afdac9589c57f658938969a27caa5
behzodkhoja <nigmatxojayevbehzodxoja@gmail.com>
2023-05-16 17:03:42 +0500

    created orderCreationDto class

--- PayOffice.Service/DTOs/Orders/OrderCreationDto.cs
+++ PayOffice.Service/DTOs/Orders/OrderCreationDto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PayOffice.Domain.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,13 @@ namespace PayOffice.Service.DTOs.Orders
 {
     public class OrderCreationDto
     {
+        public string CompanyName { get; set; }
+        public string AgentName { get; set; }
+        public string AgentPhone { get; set; }
+        public string OrderNames { get; set; }
+        public decimal Price { get; set; }
+        public OrderPaymentType PaymentType { get; set; }
+        public DateTime InComeDay { get; set; }
+        public OrderStatus Status { get; set; }
     }
 }
